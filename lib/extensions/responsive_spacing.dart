2ce010aa1524f549fd53cb10c377d073728f29ae
Emanuele Valsecchi <emanuelevalsecchi00@gmail.com>
2026-08-29 18:00:56 +0100

    Added a responsive spacing dimension

--- lib/extensions/responsive_spacing.dart
+++ lib/extensions/responsive_spacing.dart
@@ -2,12 +2,14 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 class ResponsiveSpacing extends ThemeExtension<ResponsiveSpacing> {
+  final double xs;
   final double sm; 
   final double md; 
   final double lg; 
   final double xl;
 
   const ResponsiveSpacing({
+    this.xs = 4.0,
     this.sm = 8.0, 
     this.md = 16.0, 
     this.lg = 24.0,
@@ -16,9 +18,10 @@ class ResponsiveSpacing extends ThemeExtension<ResponsiveSpacing> {
 
   @override
   ThemeExtension<ResponsiveSpacing> copyWith({
-    double? sm, double? md, double? lg, double? xl, double? xxl,
+    double? xs, double? sm, double? md, double? lg, double? xl, double? xxl,
   }) {
     return ResponsiveSpacing(
+      xs: xs ?? this.xs,
       sm: sm ?? this.sm,
       md: md ?? this.md,
       lg: lg ?? this.lg,
@@ -30,6 +33,7 @@ class ResponsiveSpacing extends ThemeExtension<ResponsiveSpacing> {
   ThemeExtension<ResponsiveSpacing> lerp(ThemeExtension<ResponsiveSpacing>? other, double t) {
     if (other is! ResponsiveSpacing) return this;
     return ResponsiveSpacing(
+      xs: ui.lerpDouble(xs, other.xs, t)!,
       sm: ui.lerpDouble(sm, other.sm, t)!,
       md: ui.lerpDouble(md, other.md, t)!,
       lg: ui.lerpDouble(lg, other.lg, t)!,
@@ -41,6 +45,7 @@ class ResponsiveSpacing extends ThemeExtension<ResponsiveSpacing> {
 extension SpacingContext on BuildContext {
   ResponsiveSpacing get responsiveSpacing => Theme.of(this).extension<ResponsiveSpacing>()!;
 
+  EdgeInsets get paddingXs => EdgeInsets.all(responsiveSpacing.xs);
   EdgeInsets get paddingSm => EdgeInsets.all(responsiveSpacing.sm);
   EdgeInsets get paddingMd => EdgeInsets.all(responsiveSpacing.md);
   EdgeInsets get paddingLg => EdgeInsets.all(responsiveSpacing.lg);
