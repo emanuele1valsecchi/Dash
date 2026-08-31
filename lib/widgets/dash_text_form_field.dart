3231af1c37c1f2dec82e4780845fb85235999d24
Emanuele Valsecchi <emanuelevalsecchi00@gmail.com>
2026-08-31 15:24:34 +0100

    Improved the Dash Text Form Field Widget for UI and UX

--- lib/widgets/dash_text_form_field.dart
+++ lib/widgets/dash_text_form_field.dart
@@ -1,22 +1,36 @@
+import 'package:dash/extensions/responsive_border_radius.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
 
 class DashTextFormField extends StatefulWidget {
-  final double widthFactor;
-  final String label;
+  final double? widthFactor;
+  final int? maxLength;
+  final BorderRadius? borderRadius;
+
+  final IconData? prefixIconSymbols;
+
+  final String? label;
+  final String? hintText;
+
   final bool largeText;
   final bool clearOption;
+  final bool charactersCounter;
 
   final TextEditingController? controller;
   final Function(String)? onChanged;
   final String? Function(String?)? validator;
 
 	const DashTextFormField({
-    super.key, 
-    required this.label,
-    this.widthFactor = 0.8, 
+    super.key,
+    this.widthFactor = 0.8,
+    this.maxLength,
+    this.borderRadius,
+    this.prefixIconSymbols,
+    this.label,
+    this.hintText,
     this.largeText = false,
     this.clearOption = false,
+    this.charactersCounter = true,
     this.controller,
     this.onChanged,
     this.validator
@@ -40,42 +54,80 @@ class _DashTextFormFieldState extends State<DashTextFormField> {
   void dispose(){
     if (widget.controller == null) {
       _textEditingController.dispose();
+    } else {
+      _textEditingController.removeListener(_handleTextChange);
     }
     super.dispose();
   }
 
 	@override
 	Widget build(BuildContext context) {
-    final bool showClearButton = widget.clearOption && _textEditingController.text.isNotEmpty;
+    Widget textFormField = _buildTextFormField(context);
 
-		return SizedBox(
-      width: MediaQuery.sizeOf(context).width * widget.widthFactor,
-      child: TextFormField(
-        textAlignVertical: TextAlignVertical.top,
-        controller: _textEditingController,
-        maxLength: widget.largeText ? 100 : 20,
-        maxLines: widget.largeText ? 4 : 1,
-        validator: widget.validator,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        obscureText: false,
-        decoration: InputDecoration(
-          alignLabelWithHint: true,
-          border: const OutlineInputBorder(),
-          labelText: widget.label,
-          suffixIcon: (showClearButton & widget.clearOption) ? 
-            IconButton(
-              icon: const Icon(Symbols.clear_rounded),
-              onPressed: _textEditingController.clear,
-            )
-          : null,
-        ),
-      ),
-    );
+    if ( widget.widthFactor != null && widget.widthFactor! < 1.0 ){
+      return SizedBox(
+        width: MediaQuery.sizeOf(context).width * widget.widthFactor!,
+        child: textFormField,
+      );
+    }
+
+    return textFormField;
 	}
 
+  Widget _buildTextFormField(BuildContext context){
+    final bool showClearButton = widget.clearOption && _textEditingController.text.isNotEmpty;
+
+    final int? effectiveMaxLength = widget.charactersCounter 
+        ? (widget.maxLength ?? (widget.largeText ? 100 : 20))
+        : widget.maxLength;
+
+    final BorderRadius effectiveRadius = widget.borderRadius ?? context.radiusMd;
+
+    return TextFormField(
+      textAlignVertical: widget.largeText 
+        ? TextAlignVertical.top 
+        : TextAlignVertical.center,
+      controller: _textEditingController,
+      maxLength: effectiveMaxLength,
+      maxLines: widget.largeText ? 4 : 1,
+      validator: widget.validator,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      obscureText: false,
+      buildCounter: widget.charactersCounter 
+        ? null 
+        : (context, {required currentLength, required isFocused, maxLength}) => null,
+      decoration: InputDecoration(
+        alignLabelWithHint: true,
+        labelText: widget.label,
+        hintText: widget.hintText,
+        hintStyle: TextStyle(color: Theme.of(context).hintColor),
+        prefixIcon: (widget.prefixIconSymbols != null ) ? Icon(widget.prefixIconSymbols) : null,
+        border: OutlineInputBorder(
+          borderRadius: effectiveRadius,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: effectiveRadius,
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.outlineVariant),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: effectiveRadius,
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: 1.5,
+          ),
+        ),
+        suffixIcon: showClearButton ? 
+          IconButton(
+            icon: const Icon(Symbols.clear_rounded),
+            onPressed: _textEditingController.clear,
+          )
+        : null,
+      ),
+    );
+  }
+
   void _handleTextChange(){
-    setState(() {
-    });
+    setState(() {});
     
     if (widget.onChanged != null) {
       widget.onChanged!(_textEditingController.text);
